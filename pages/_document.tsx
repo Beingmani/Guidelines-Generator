04c195f8c39eed4bce55f62d8eac4f01c6afed43
Manivannan <a1842066@adelaide.edu.au>
2023-03-24 12:11:56 +1030

    Added anlytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,30 +5,31 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+        <script async src="https://cdn.splitbee.io/sb.js"></script>
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="Generate your next Twitter bio in seconds."
+            content="Generate your next desig guidelines in minutes"
           />
-          <meta property="og:site_name" content="twitterbio.com" />
+          <meta property="og:site_name" content="guideline Generator" />
           <meta
             property="og:description"
-            content="Generate your next Twitter bio in seconds."
+            content="Generate your next desig guidelines in mnutes."
           />
-          <meta property="og:title" content="Twitter Bio Generator" />
+          <meta property="og:title" content="Guidelines Generator" />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Twitter Bio Generator" />
+          <meta name="twitter:title" content="Guidelines Generator" />
           <meta
             name="twitter:description"
-            content="Generate your next Twitter bio in seconds."
+            content="Generate your next desig guidelines in minutes"
           />
           <meta
             property="og:image"
-            content="https://twitterbio.com/og-image.png"
+            // content="https://twitterbio.com/og-image.png"
           />
           <meta
             name="twitter:image"
-            content="https://twitterbio.com/og-image.png"
+            // content="https://twitterbio.com/og-image.png"
           />
         </Head>
         <body>
